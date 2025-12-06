bac0e02005960b76b696070839b1627c8c6ab003
Sai Srikar <saisrikarraokattekola@gmail.com>
2025-12-06 18:09:03 +0530

    Updated Url

--- src/store/Api.js
+++ src/store/Api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1/products",
+  baseURL: "https://cafe-horizon-server.vercel.app/api/v1/products",
 });
 
 // --------------------- REQUEST INTERCEPTOR ---------------------
